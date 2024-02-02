19f98f1e52a90218f0bf95f9300fb0a7adee7df9
zhoucheng <zhoucheng133@outlook.com>
2024-02-02 15:39:36 +0800

    调整窗口央视

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 
@@ -12,16 +14,44 @@ void main() {
   });
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Container(
+                height: 30,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.red,
+                child: WindowTitleBarBox(
+                  child: MoveWindow(),
+                ),
+              ),
+            ),
+            // Column(
+            //   children: [
+            //     Text("hello world!")
+            //   ],
+            // )
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Text("hello world!")
+            )
+          ],
+        )
       ),
     );
   }
