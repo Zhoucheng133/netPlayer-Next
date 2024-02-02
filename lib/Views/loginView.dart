648d98e2a9444a1fd622d85fd21ad9aefb708c8c
zhoucheng <zhoucheng133@outlook.com>
2024-02-02 17:00:01 +0800

    添加登陆界面的底板

--- lib/Views/loginView.dart
+++ lib/Views/loginView.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names, camel_case_types, prefer_const_constructors
+// ignore_for_file: file_names, camel_case_types, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_alert/flutter_platform_alert.dart';
@@ -30,13 +30,26 @@ Future<void> showDialog() async {
 class _loginViewState extends State<loginView> {
   @override
   Widget build(BuildContext context) {
-    return  Center(
-      child: TextButton(
-        onPressed: (){
-          showDialog();
-        }, 
-        child: Text("测试按钮")
-      ),  
+    return  Container(
+      color: Color.fromARGB(255, 240, 240, 240),
+      child: Center(
+        child: Container(
+          width: 500,
+          height: 400,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromARGB(150, 200, 200, 200),
+                offset: Offset(0, 0),
+                blurRadius: 10.0,
+                spreadRadius: 1.0,
+              ),
+            ],
+          ),
+        ),  
+      ),
     );
   }
 }
