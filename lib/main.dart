b08e9b82e2982007f843c8496f2fc560f5b69c2d
zhoucheng <zhoucheng133@outlook.com>
2024-02-05 16:01:15 +0800

    创建为FluentApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, sized_box_for_whitespace
 
-import 'package:flutter/material.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+// import 'package:flutter/material.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:netplayer_next/Views/loginView.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
   doWhenWindowReady(() {
     appWindow.minSize = Size(1100, 770);
     appWindow.size = Size(1100, 770);
@@ -13,6 +14,17 @@ void main() {
     appWindow.title="netPlayer Next";
     appWindow.show();
   });
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FluentApp(
+      home: MainApp(),
+    );
+  }
 }
 
 class MainApp extends StatefulWidget {
@@ -25,29 +37,25 @@ class MainApp extends StatefulWidget {
 class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Stack(
-          children: [
-            Positioned(
-              top: 0,
-              left: 0,
-              child: SizedBox(
-                height: 30,
-                width: MediaQuery.of(context).size.width,
-                child: WindowTitleBarBox(
-                  child: MoveWindow(),
-                ),
-              ),
+    return Stack(
+      children: [
+        Positioned(
+          top: 0,
+          left: 0,
+          child: SizedBox(
+            height: 30,
+            width: MediaQuery.of(context).size.width,
+            child: WindowTitleBarBox(
+              child: MoveWindow(),
             ),
-            AnimatedContainer(
-              duration: Duration(milliseconds: 300),
-              color: Color.fromARGB(255, 240, 240, 240),
-              child: loginView(),
-            )
-          ],
+          ),
+        ),
+        AnimatedContainer(
+          duration: Duration(milliseconds: 300),
+          color: Color.fromARGB(255, 240, 240, 240),
+          child: loginView(),
         )
-      ),
+      ],
     );
   }
 }
