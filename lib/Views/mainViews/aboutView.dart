cc64db8b50d5d25f9b2749970b0fcb0bc27b9016
Zhoucheng133 <zhoucheng133@outlook.com>
2024-02-07 18:59:54 +0800

    完成关于界面layout

--- lib/Views/mainViews/aboutView.dart
+++ lib/Views/mainViews/aboutView.dart
@@ -1,6 +1,10 @@
-// ignore_for_file: camel_case_types, file_names, prefer_const_constructors
+// ignore_for_file: camel_case_types, file_names, prefer_const_constructors, unnecessary_brace_in_string_interps, prefer_const_literals_to_create_immutables
 
-import 'package:fluent_ui/fluent_ui.dart';
+// import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
+import '../components/dialogs.dart';
 
 class aboutView extends StatefulWidget {
   const aboutView({super.key});
@@ -10,6 +14,33 @@ class aboutView extends StatefulWidget {
 }
 
 class _aboutViewState extends State<aboutView> {
+
+  String version="";
+
+  Future<void> getVersion() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    setState(() {
+      version=packageInfo.version;
+    });
+  }
+
+  void showInGit(){
+    // TODO 打开GitHub仓库
+  }
+
+  void showLicense(BuildContext context){
+    showLicenseDialog(context);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getVersion();
+  }
+
+  bool hoverGit=false;
+  bool hoverLicense=false;
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -19,6 +50,66 @@ class _aboutViewState extends State<aboutView> {
           Image.asset(
             "assets/icon.png",
             width: 200,
+          ),
+          SizedBox(height: 10,),
+          Text(
+            "netPlayer",
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 5,),
+          Text(
+            "Next v${version}",
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          SizedBox(height: 20,),
+          // Text(
+          //   "Developed by zhouc",
+          // ),
+          // SizedBox(height: 20,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () => showInGit(),
+                child: MouseRegion(
+                  onEnter: (event) => setState(() { hoverGit=true; }),
+                  onExit: (event) => setState(() { hoverGit=false; }),
+                  cursor: SystemMouseCursors.click,
+                  child: AnimatedDefaultTextStyle(
+                    duration: Duration(milliseconds: 200),
+                    style: TextStyle(
+                      color: hoverGit ? Colors.blue : Colors.grey[400],
+                    ),
+                    child: Text(
+                      "在GitHub中查看",
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(width: 20,),
+              GestureDetector(
+                onTap: () => showLicense(context),
+                child: MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  onEnter: (event) => setState(() { hoverLicense=true; }),
+                  onExit: (event) => setState(() { hoverLicense=false; }),
+                  child: AnimatedDefaultTextStyle(
+                    duration: Duration(milliseconds: 200),
+                    style: TextStyle(
+                      color: hoverLicense ? Colors.blue : Colors.grey[400],
+                    ),
+                    child: Text(
+                      "关于License",
+                    ),
+                  ),
+                ),
+              )
+            ],
           )
         ],
       ),
