6a1a4d560a2430b377fcf4a45554907ffe249713
Zhoucheng133 <zhoucheng133@outlook.com>
2024-02-07 18:59:37 +0800

    添加确保Widget运行完成

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'functions/request.dart';
 import 'paras/paras.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
   doWhenWindowReady(() {
     appWindow.minSize = Size(1100, 770);
