b4c3b15b175a47ac2e8e4b5096ad8e48fed36f19
Zhoucheng133 <zhoucheng133@outlook.com>
2024-02-08 19:00:11 +0800

    添加所有歌曲赋值

--- lib/Views/mainViews/allSongsView.dart
+++ lib/Views/mainViews/allSongsView.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: camel_case_types, file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:net_player_next/Views/components/tableHeader.dart';
 import 'package:net_player_next/Views/components/titleBar.dart';
 
 import '../../functions/request.dart';
+import '../../paras/paras.dart';
 
 class allSongsView extends StatefulWidget {
   const allSongsView({super.key});
@@ -15,6 +17,8 @@ class allSongsView extends StatefulWidget {
 
 
 class _allSongsViewState extends State<allSongsView> {
+
+  final Controller c = Get.put(Controller());
 
   void search(value){
     // TODO 搜索歌曲
@@ -46,7 +50,7 @@ class _allSongsViewState extends State<allSongsView> {
     var resp=await allSongsRequest();
     
     if(resp["status"]=="ok"){
-
+      c.updateAllSongs(resp["randomSongs"]["song"]);
     }else{
       warning("请求所有歌曲失败", "请检查互联网连接");
     }
@@ -67,8 +71,9 @@ class _allSongsViewState extends State<allSongsView> {
       padding: const EdgeInsets.fromLTRB(20,30,20,20),
       child: Column(
         children: [
-          // TODO 注意传递副标题
-          titleBox(searchController: search, title: "所有歌曲", subtitle: "合计x首歌", controller: searchInput, reloadList: () => reload(),),
+          Obx(() => 
+            titleBox(searchController: search, title: "所有歌曲", subtitle: "合计${c.allSongs.length}首歌", controller: searchInput, reloadList: () => reload(),),
+          ),
           SizedBox(height: 10,),
           songsHeader(),
           Expanded(
