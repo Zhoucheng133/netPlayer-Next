815e1b275178ddcf8c2bccfb7bcfbd978ca419e4
Zhoucheng133 <zhoucheng133@outlook.com>
2024-02-08 16:26:39 +0800

    添加listItem

---
+++ lib/Views/components/listItems.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: prefer_const_constructors, camel_case_types, file_names
+
+import 'package:flutter/material.dart';
+
+class songItem extends StatefulWidget {
+  const songItem({super.key});
+
+  @override
+  State<songItem> createState() => _songItemState();
+}
+
+class _songItemState extends State<songItem> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      
+    );
+  }
+}
