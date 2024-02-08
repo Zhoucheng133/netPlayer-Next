f8de72d30286ee47b2708e8c60e6ffd33c08fd90
Zhoucheng133 <zhoucheng133@outlook.com>
2024-02-08 16:57:43 +0800

    完成歌单信息展示

--- lib/Views/mainViews/playListView.dart
+++ lib/Views/mainViews/playListView.dart
@@ -2,8 +2,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:net_player_next/functions/request.dart';
 
 import '../../paras/paras.dart';
+import '../components/tableHeader.dart';
+import '../components/titleBar.dart';
 
 class playListView extends StatefulWidget {
   const playListView({super.key});
@@ -15,11 +18,58 @@ class playListView extends StatefulWidget {
 class _playListViewState extends State<playListView> {
 
   final Controller c = Get.put(Controller());
+
+  var list=[];
+  var title="";
+
+    void search(value){
+    // TODO 搜索歌曲
+    print(value);
+  }
+
+  void reload(){
+    // TODO 刷新列表
+  }
+  TextEditingController searchInput=TextEditingController();
+  
+
+  Future<void> getPlayList() async {
+    if(c.nowPage["name"]=="歌单" && c.nowPage["id"]!.isNotEmpty){
+      var resp=await playListRequest(c.nowPage["id"]!);
+      try {
+        setState(() {
+          title=resp["name"];
+          list=resp["entry"];
+        });
+      } catch (_) {}
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    ever(c.nowPage, (callback){
+      getPlayList();
+    });
+  }
   
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Obx(() => Text("歌单: ${c.nowPage['id']}")),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20,30,20,20),
+      child: Column(
+        children: [
+          // TODO 注意传递副标题
+          titleBox(searchController: search, title: title, subtitle: "合计${list.length}首歌", controller: searchInput, reloadList: () => reload(),),
+          SizedBox(height: 10,),
+          songsHeader(),
+          Expanded(
+            // 歌曲列表显示在这里
+            child: Container(),
+          )
+        ],
+      ),
     );
   }
 }
