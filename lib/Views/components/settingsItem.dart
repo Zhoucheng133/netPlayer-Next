96c4ebcc9707cc9bf865144e18ef4232cddfc1a9
Zhoucheng133 <zhoucheng133@outlook.com>
2024-02-09 09:10:12 +0800

    添加设置的switchItem

---
+++ lib/Views/components/settingsItem.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: file_names, camel_case_types, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class switchItem extends StatefulWidget {
+  
+  final bool value;
+  final String text;
+  final ValueChanged setValue;
+
+  const switchItem({super.key, required this.value, required this.text, required this.setValue});
+
+  @override
+  State<switchItem> createState() => _switchItemState();
+}
+
+class _switchItemState extends State<switchItem> {
+
+  Color getSwitchColor(Set<MaterialState> states){
+    return Colors.white;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          widget.text,
+          textAlign: TextAlign.left,
+        ),
+        SizedBox(width: 5,),
+        Transform.scale(
+          scale: 0.8,
+          child: Switch(
+            hoverColor: Colors.white,
+            thumbColor: MaterialStateProperty.resolveWith(getSwitchColor),
+            splashRadius: 0,
+            onChanged: (bool value) { 
+              setState(() {
+                widget.setValue(value);
+              });
+            }, 
+            value: widget.value,
+          ),
+        )
+      ],
+    );
+  }
+}
