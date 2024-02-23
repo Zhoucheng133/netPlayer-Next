f80108f0c9823483d3f6b898418c0f7a8ef569f3
Zhoucheng133 <zhoucheng133@outlook.com>
2024-02-23 11:37:19 +0800

    删除不需要的导入

--- lib/Views/mainView.dart
+++ lib/Views/mainView.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: camel_case_types, file_names, prefer_const_constructors
 
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hotkey_manager/hotkey_manager.dart';
