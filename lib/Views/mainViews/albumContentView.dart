0df1ed4a5e0fbbc24d29deab20873ba30ae9ce14
Zhoucheng133 <zhoucheng133@outlook.com>
2024-02-24 16:38:35 +0800

    完成显示专辑信息

--- lib/Views/mainViews/albumContentView.dart
+++ lib/Views/mainViews/albumContentView.dart
@@ -2,9 +2,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:net_player_next/Views/components/tableHeader.dart';
 import 'package:net_player_next/functions/operations.dart';
 
 import '../../paras/paras.dart';
+import '../components/listItems.dart';
 import '../components/titleBar.dart';
 
 class albumContentView extends StatefulWidget {
@@ -39,13 +41,56 @@ class _albumContentViewState extends State<albumContentView> {
     getData();
   }
 
+  final ScrollController controller=ScrollController();
+
+  void playFromAlbum(int index){
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(20,30,20,20),
       child: Column(
         children: [
-          titleBoxWithBack(title: "专辑: $title", subtitle: "含有歌曲${list.length}首",)
+          titleBoxWithBack(title: "专辑: $title", subtitle: "含有歌曲${list.length}首",),
+          SizedBox(height: 10,),
+          songsHeader(),
+          Expanded(
+            child: ListView.builder(
+              controller: controller,
+              itemCount: list.length,
+              itemBuilder: (BuildContext context, int index){
+                return index==list.length-1 ? Column(
+                  children: [
+                    songItem(
+                      artist: list[index]["artist"], 
+                      duration: list[index]["duration"], 
+                      index: index, 
+                      title: list[index]["title"], 
+                      isLoved: operations().isLoved(list[index]["id"]), 
+                      playSong: ()=>playFromAlbum(index), 
+                      isPlaying: false, 
+                      id: list[index]["id"], 
+                      silentReload: () {},
+                    ),
+                    SizedBox(height: 120,),
+                  ],
+                ) :
+                songItem(
+                  artist: list[index]["artist"], 
+                  duration: list[index]["duration"], 
+                  index: index, 
+                  title: list[index]["title"], 
+                  isLoved: operations().isLoved(list[index]["id"]), 
+                  playSong: ()=>playFromAlbum(index), 
+                  isPlaying: false, 
+                  id: list[index]["id"],
+                  silentReload: () {},
+                );
+              }
+            ),
+          )
         ],
       ),
     );
