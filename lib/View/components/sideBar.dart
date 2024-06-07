daa8320bd0d39e7ce216b6504ce6c2cd9f9ef853
周诚 <zhoucheng133@outlook.com>
2024-06-07 17:04:58 +0800

    完成左边栏基本按钮

--- lib/View/components/sideBar.dart
+++ lib/View/components/sideBar.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names, camel_case_types
 
 import 'package:flutter/material.dart';
+import 'package:net_player_next/View/components/sideBarItem.dart';
 
 class sideBar extends StatefulWidget {
   const sideBar({super.key});
@@ -12,6 +13,16 @@ class sideBar extends StatefulWidget {
 class _sideBarState extends State<sideBar> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Padding(
+      padding: EdgeInsets.only(top: 10),
+      child: Column(
+        children: [
+          sideBarItem(name: '专辑', icon: Icons.album_rounded, index: 2,),
+          sideBarItem(name: '艺人', icon: Icons.mic_rounded, index: 1,),
+          sideBarItem(name: '所有歌曲', icon: Icons.queue_music_rounded, index: 0,),
+          sideBarItem(name: '搜索', icon: Icons.search_rounded, index: 4,)
+        ],
+      ),
+    );
   }
 }
