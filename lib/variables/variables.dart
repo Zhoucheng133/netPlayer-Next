12e819ba6cc1a1243ae5dec1734d7d6f41f172f3
周诚 <zhoucheng133@outlook.com>
2024-06-07 18:01:32 +0800

    添加一些变量

--- lib/variables/variables.dart
+++ lib/variables/variables.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 class Controller extends GetxController{
   // 当前页面
@@ -15,8 +16,16 @@ class Controller extends GetxController{
     '歌单',     // id: String?
     '搜索'      // id: String?
   ];
+  // 用户信息
   var userInfo={
+    'url': null,      // String?
     'username': null, // String?
-    'password': null, // String?
-  };
+    'salt': null,     // String?
+    'token': null,    // String?
+  }.obs;
+  // 一些颜色样本，从浅色=>深色
+  var color1=const Color.fromRGBO(248, 249, 255, 1);
+  var color2=const Color.fromARGB(255, 238, 241, 255);
+  var color3=const Color.fromARGB(255, 233, 236, 255);
+  var color4=const Color.fromARGB(255, 179, 189, 252);
 }
