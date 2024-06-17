4bfe531115e41a9a8cf72f0cece934fe2b5ed8ec
周诚 <zhoucheng133@outlook.com>
2024-06-17 19:42:41 +0800

    完成歌词界面layout

--- lib/View/mainViews/lyric.dart
+++ lib/View/mainViews/lyric.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: camel_case_types
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:net_player_next/View/functions/operations.dart';
+import 'package:net_player_next/variables/variables.dart';
 
 class lyricView extends StatefulWidget {
   const lyricView({super.key});
@@ -11,17 +13,58 @@ class lyricView extends StatefulWidget {
 }
 
 class _lyricViewState extends State<lyricView> {
+
+  bool hoverBack=false;
+  final Controller c = Get.put(Controller());
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Center(
-        child: IconButton(
-          onPressed: (){
-            Operations().toggleLyric(context);
-          }, 
-          icon: const Icon(Icons.arrow_left_rounded)
-        ),
+      child: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container()
+                ),
+                Expanded(
+                  child: Container()
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 50,
+            child: GestureDetector(
+              onTap: (){
+                Operations().toggleLyric(context);
+              },
+              child: MouseRegion(
+                onEnter: (_){
+                  setState(() {
+                    hoverBack=true;
+                  });
+                },
+                onExit: (_){
+                  setState(() {
+                    hoverBack=false;
+                  });
+                },
+                cursor: SystemMouseCursors.click,
+                child: TweenAnimationBuilder(
+                  tween: ColorTween(end: hoverBack ? c.color6 : c.color4), 
+                  duration: const Duration(milliseconds: 200),
+                  builder: (_, value, __)=>Icon(
+                    Icons.arrow_downward_rounded,
+                    color: value,
+                  )
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
