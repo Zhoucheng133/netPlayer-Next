758e483d79f77f7211e0dcbf3367b7cc36a7623f
zhoucheng <zhoucheng133@outlook.com>
2025-02-22 04:01:12 +0800

    修改版本号显示方式

--- lib/variables/variables.dart
+++ lib/variables/variables.dart
@@ -9,8 +9,6 @@ import 'package:smtc_windows/smtc_windows.dart';
 class Controller extends GetxController{
   // 是否使用桌面歌词, 仅Windows
   bool useDesktopLyric=true;
-  // 当前软件版本
-  String version='3.4.3';
   // 当前页面索引
   RxInt pageIndex=0.obs;
   // 当前页面Id
