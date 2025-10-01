91e1b9fe047f05faf449ea9c27443d64c5349f5d
zhoucheng <zhoucheng133@outlook.com>
2025-10-01 19:58:40 +0800

    拆分queue项

--- lib/views/components/play_queue.dart
+++ lib/views/components/play_queue.dart
@@ -4,6 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:net_player_next/variables/color_controller.dart';
 import 'package:net_player_next/variables/song_controller.dart';
 import 'package:net_player_next/variables/variables.dart';
+import 'package:net_player_next/views/components/queue_item.dart';
 import 'package:scroll_to_index/scroll_to_index.dart';
 
 class PlayQueue extends StatefulWidget {
@@ -100,40 +101,7 @@ class _PlayQueueState extends State<PlayQueue> {
                   key: ValueKey(index),
                   controller: controller,
                   index: index,
-                  child: SizedBox(
-                    height: 30,
-                    child: Row(
-                      children: [
-                        SizedBox(
-                          width: 30,
-                          child: Center(
-                            child: songController.nowPlay.value.list[index].id==songController.nowPlay.value.id ? Icon(
-                              Icons.play_arrow_rounded,
-                              color: colorController.color6(),
-                              size: 15,
-                            ) : Text(
-                              (index+1).toString(),
-                              style: GoogleFonts.notoSansSc(
-                                fontSize: 13,
-                                color: colorController.darkMode.value ? Colors.white : Colors.black,
-                              ),
-                            ),
-                          )
-                        ),
-                        const SizedBox(width: 10,),
-                        Expanded(
-                          child: Text(
-                            songController.nowPlay.value.list[index].title,
-                            overflow: TextOverflow.ellipsis,
-                            style: GoogleFonts.notoSansSc(
-                              color: songController.nowPlay.value.list[index].id==songController.nowPlay.value.id ? colorController.color6(): colorController.darkMode.value ? Colors.white : Colors.black,
-                              fontWeight: songController.nowPlay.value.list[index].id==songController.nowPlay.value.id ? FontWeight.bold : FontWeight.normal
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                  child: QueueItem(index: index)
                 )
               ),
             )
---
+++ lib/views/components/queue_item.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:net_player_next/variables/color_controller.dart';
+import 'package:net_player_next/variables/song_controller.dart';
+import 'package:net_player_next/variables/variables.dart';
+
+class QueueItem extends StatefulWidget {
+  final int index;
+
+  const QueueItem({super.key, required this.index});
+
+  @override
+  State<QueueItem> createState() => _QueueItemState();
+}
+
+class _QueueItemState extends State<QueueItem> {
+
+  final SongController songController=Get.find();
+  final ColorController colorController=Get.find();
+  final Controller c=Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 30,
+      child: Row(
+        children: [
+          SizedBox(
+            width: 30,
+            child: Center(
+              child: songController.nowPlay.value.list[widget.index].id==songController.nowPlay.value.id ? Icon(
+                Icons.play_arrow_rounded,
+                color: colorController.color6(),
+                size: 15,
+              ) : Text(
+                (widget.index+1).toString(),
+                style: GoogleFonts.notoSansSc(
+                  fontSize: 13,
+                  color: colorController.darkMode.value ? Colors.white : Colors.black,
+                ),
+              ),
+            )
+          ),
+          const SizedBox(width: 10,),
+          Expanded(
+            child: Text(
+              songController.nowPlay.value.list[widget.index].title,
+              overflow: TextOverflow.ellipsis,
+              style: GoogleFonts.notoSansSc(
+                color: songController.nowPlay.value.list[widget.index].id==songController.nowPlay.value.id ? colorController.color6(): colorController.darkMode.value ? Colors.white : Colors.black,
+                fontWeight: songController.nowPlay.value.list[widget.index].id==songController.nowPlay.value.id ? FontWeight.bold : FontWeight.normal
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
